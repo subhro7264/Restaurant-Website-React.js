7798d4ba750fcc2b88e0cac255f4177c24286aef
subhro7264 <subhrohazra7.com>
2023-08-18 16:57:21 +0530

    Adding a Form

--- restaurant-app/src/components/Meals/Available.js
+++ restaurant-app/src/components/Meals/Available.js
@@ -1,5 +1,6 @@
 import classes from "./AvailableMeals.module.css";
-
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -27,16 +28,16 @@ const DUMMY_MEALS = [
   },
 ];
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <>
-      <li className={classes.name}>{meal.name}</li>{" "}
-      <li className={classes.description}>{meal.description}</li>{" "}
-      <li className={classes.price}>${meal.price}</li> <hr />{" "}
-    </>
-  ));
+  const mealsList = DUMMY_MEALS.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
+
+);
   return (
     <section className={classes.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+       <ul>
+        {mealsList}  
+     </ul>
+     </Card>
     </section>
   );
 };
