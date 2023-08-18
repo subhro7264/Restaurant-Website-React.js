31ea7f081d0908c9866f22f7cd5f4cda5f4bad36
subhro7264 <subhrohazra7.com>
2023-08-18 14:50:47 +0530

    Making the Items visible

--- restaurant-app/src/components/Meals/Meals.js
+++ restaurant-app/src/components/Meals/Meals.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./Available";
 
 const Meals = () => {
   return (
     <Fragment>
       <MealsSummary />
+      <AvailableMeals />
     </Fragment>
   );
 };
