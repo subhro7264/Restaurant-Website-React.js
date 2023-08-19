7709164c41556e73e5cf707844d01d0dedd00515
subhro7264 <subhrohazra7.com>
2023-08-19 10:27:28 +0530

    v

--- restaurant-app/src/components/Layout/HeaderCartButton.js
+++ restaurant-app/src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
-
+import CardContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton=(props)=>{
+
+   const cartCtx= useContext(CardContext);
+   const numberOfCartItems=cartCtx.items.reduce((curNumber,item)=>{
+    return curNumber+item.amount;
+   },0);
     return(
    <button  className={classes.button} onClick={props.onClick}>
     <span className={classes.icon}>
@@ -11,7 +16,7 @@ const HeaderCartButton=(props)=>{
     </span>
     <span>Your Cart</span>
     <span className={classes.badge}>
-       0 
+       {numberOfCartItems}
     </span>
 </button>
     )
